d14d5bbb0be01631d7961c8cd0c7457f99d9dfc3
Ziv <ziv.israeli01@gmail.com>
2019-12-20 14:42:41 -0800

    Updates

---
+++ precache-manifest.0cd5d96ec9173f9c0c64c45210860f61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13e33cabba8a13449cf96feff40ff99b",
+    "url": "/mia-react/index.html"
+  },
+  {
+    "revision": "0f6913f6d268cf83a4ea",
+    "url": "/mia-react/static/js/2.c5f6ac44.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mia-react/static/js/2.c5f6ac44.chunk.js.LICENSE"
+  },
+  {
+    "revision": "db2d87d574a70a916daa",
+    "url": "/mia-react/static/js/main.123991db.chunk.js"
+  },
+  {
+    "revision": "fc1c766b071e04afba1e",
+    "url": "/mia-react/static/js/runtime-main.a884c96e.js"
+  }
+]);

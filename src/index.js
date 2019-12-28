e96feb3a56d5221aef457a095926041eaebdc6f7
Ziv <ziv.israeli01@gmail.com>
2019-12-28 14:21:35 -0800

    Initial files

--- src/index.js
+++ src/index.js
@@ -1,31 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App.js'
 
-let jsContainer = document.getElementById('js');
-
-const render = () => {
-  jsContainer.innerHTML = `
-  <div class="demo">
-    Hello JS
-    <input />
-    <p>${new Date()}</p>
-  </div>
-`;
-
-  let reactContainer = document.getElementById('react');
-  ReactDOM.render(
-    React.createElement(
-      "div", { className: "demo" },
-      "Hello React",
-      React.createElement("input"),
-      React.createElement(
-        "p",
-        null,
-        new Date().toString()
-      )
-    ),
-    reactContainer
-  )
-}
-
-setInterval(render, 1000);
+ReactDOM.render(<App />, 
+                document.getElementById('root'));

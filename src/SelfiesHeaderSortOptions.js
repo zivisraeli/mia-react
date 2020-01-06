154764db29c91eff98f505da1509ab40c925198f
Ziv <ziv.israeli01@gmail.com>
2020-01-06 14:49:07 -0800

    More new components

---
+++ src/SelfiesHeaderSortOptions.js
@@ -0,0 +1,92 @@
+import React from 'react';
+
+import { setSortProperties, getCookie } from './Utils.js';
+
+class SelfiesHeaderSortOptions extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      visibility: 'hidden'
+    };
+  }
+
+  componentDidMount() {
+    this.readSortCookie();
+  }
+
+  // =============================================================================
+  // readSortCookie would:
+  // - read 'sort' cookie 
+  // - set the selected option.
+  // - make the select-option-div visible (to avoid jitter).
+  // - sort the grid items array.
+  // =============================================================================
+  readSortCookie = () => {
+    // set default values in case there is no cookie yet.
+    let sortAttr = 'likes';
+    let sortDirection = -1;
+    let sortCookie = getCookie('sort');
+    if (sortCookie === null) {
+      sortCookie = "likes-1";
+    } else {
+      let sortArr = sortCookie.match('(.*)([-+]1)');
+      sortAttr = sortArr[1];
+      sortDirection = sortArr[2];
+    }
+
+    // Find the select option element pointed to by the cookie and add 'selected' attribute.
+    // The options carry the same id as the cookie.
+    let selectedOption = document.getElementById(sortCookie);
+    selectedOption.setAttribute("selected", "selected");
+    this.setState({ visibility: 'visible' });
+
+    setSortProperties(sortAttr, sortDirection);
+  }
+
+  sortChangeEvent = (event) => {
+    let sortAttr = '';
+    let sortDirection = '';
+    let selectedIndex = event.target.selectedIndex;
+    let selectedOptionId = event.target[selectedIndex].id;
+    switch (selectedOptionId) {
+      case 'likes+1':
+        sortAttr = 'likes';
+        sortDirection = 1;
+        break;
+      case 'likes-1':
+        sortAttr = 'likes';
+        sortDirection = 1;
+        break;
+      case 'captions+1':
+        sortAttr = 'captions';
+        sortDirection = 1;
+        break;
+      case 'captions-1':
+        sortAttr = 'captions';
+        sortDirection = -1;
+        break;
+      default:
+        break;
+    }
+
+    setSortProperties(sortAttr, sortDirection);
+    //setCookie('sort', selectedOptionId);
+  }
+
+  render() {
+    let theStyle = { visibility: this.state.visibility }
+
+    return (<div id="select-option-div" style={theStyle}>
+            <select id="select-sort" onChange={this.sortChangeEvent}>
+              <option id="likes+1">Like number; Asc.</option>
+              <option id="likes-1">Like number; Desc.</option>
+              <option id="captions+1">Caption; Asc.</option>
+              <option id="captions-1">Caption; Desc.</option>
+            </select>
+	         </div>);
+  }
+}
+
+export default SelfiesHeaderSortOptions;
---
+++ src/Utils.js
@@ -0,0 +1,87 @@
+import { gridItemsData } from './gridItemsData';
+
+// =============================================================================
+// This function takes 2 parameters, compare them and return true or false.
+// Javascript sort function take a "compare" function as a parameter. 
+// Since gridItemsData is imported, Javascript would allow to change it.
+// An imported symbol is akin to having a const symbol. 
+// =============================================================================
+export const sortGridItemsEvent = (sortByAttr, direction) => {
+  let sortedGridItems = gridItemsData.sort((item1, item2) => {
+    let retVal = 0;
+    if (sortByAttr === "captions") {
+      retVal = item1.caption.toUpperCase() > item2.caption.toUpperCase() ? 1 : -1;
+    } else {
+      retVal = item1.likeCount > item2.likeCount ? 1 : -1
+    }
+    return retVal * direction;
+  });
+  
+  sortedGridItems.forEach( (gridItem, index) => {
+    gridItemsData[index] = gridItem;
+  });
+}
+
+// =============================================================================
+// This method is invoked from various places.
+// Upong setting the sort attribute and direction 
+// the gridItemsData array should be re-sorted.
+// =============================================================================  
+export const setSortProperties = (sortAttr, sortDirection) => {
+  sortGridItemsEvent(sortAttr, sortDirection);
+}
+
+// =============================================================================
+// cookie related functions
+// =============================================================================
+export const getCookie = (name) => {
+  // since document.cookie returns all cookie, match would filter out the one I need.
+  // the match regex: cookie name should follow an equal sign AND NOT a space of a semi-colon.
+  // with 'capturing groups' we get an array with the array[0] is the entire expression value.
+  // the rest of the values are the group's value.
+  let value = document.cookie.match('(?:^|;)\\s?' + name + '=([^\\s;]*)');
+  return value ? value[1] : null;
+}
+
+/*export const setCookie = (name, value, days = 365) => {
+  let d = new Date;
+  d.setTime(d.getTime() + 24 * 60 * 60 * 1000 * days);
+  document.cookie = name + "=" + value + ";path=/;expires=" + d.toGMTString();
+}
+*/
+// =============================================================================
+// Determine the current slected menu and style it differently. 
+// =============================================================================
+export const currentMenuEntry = () => {
+  let anchor = '';
+  let pageName = window.location.pathname;
+  if (pageName.includes("index.html")) {
+    anchor = document.querySelector('nav span#index');
+  } else if (pageName.includes("selfies.html")) {
+    anchor = document.querySelector('nav span#selfies');
+  } else if (pageName.includes("fans.html")) {
+    anchor = document.querySelector('nav span#fans');
+  } else if (pageName.includes("kitbull.html")) {
+    anchor = document.querySelector('nav span#kitbull');
+  } else {
+    anchor = document.querySelector('nav span#index');
+  }
+  anchor.style.textShadow = '1px 1px 4px gray';
+}
+
+// =============================================================================
+// Based on a cookie value find the header img id and set the image accordingly.
+// =============================================================================
+export const setHeaderImage = () => {
+  let headerImgId = getCookie("headerImgId");
+  headerImgId = headerImgId === null ? 'small-id02' : headerImgId;
+  let headerImgSrc = `images/mia-${headerImgId}.jpg`;
+  let headerImgElem = document.querySelector("header img");
+  headerImgElem.setAttribute("src", headerImgSrc);
+
+  // Without this function the border will be drawn first as a straight line and then 
+  // the image would appear. I'm waiting for the image to be loaded frist. 
+  headerImgElem.onload = function() {
+    headerImgElem.style.border = "1px solid black";
+  }
+}
